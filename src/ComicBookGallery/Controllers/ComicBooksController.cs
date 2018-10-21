7497b6e57a1a3235c1ab54e9e68b9d3e53b1b941
IcemanXChill <tmega4420@gmail.com>
2018-10-21 17:27:26 -0400

    Added a comics view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from your controller!";
+            return View();
         }
     }
 }
